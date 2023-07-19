f0bdf8bea8ee25d1efc32056a5e64edccdb9f5c7
David Dobrowski <dobrowski@gmail.com>
2023-07-19 13:43:04 -0700

    Initial graphs

---
+++ TAMO.R
@@ -0,0 +1,66 @@
+
+
+library(tidyverse)
+library(janitor)
+library(MCOE)
+library(here)
+library(ggthemes)
+
+
+con <- mcoe_sql_con()
+
+
+
+
+
+TAMO <- tbl(con, "TEACHING") %>% 
+    filter(County_Name == "Monterey",
+           Aggregate_Level == "C" | Aggregate_Level == "D" ,
+           Charter_School == "No",
+           DASS == "All",
+           Teacher_Experience_Level == "All",
+           Teacher_Credential_Level == "All",
+    #       School_Grade_Span == "All"
+           #        DistrictCode == "10272",
+   #        YEAR  == max(YEAR)
+    ) %>%
+           head(20000) %>%
+    collect() 
+
+
+
+# By Subject
+
+TAMO %>%
+    filter(
+        Aggregate_Level == "C",
+        School_Grade_Span == "ALL",
+        Academic_Year == "2021-22"
+        ) %>%
+    lollipop(Clear_FTE_percent,
+             Subject_Area,
+             "steel blue") +
+    labs(x = "",
+         y = "",
+         color ="",
+         title = ("Teaching Assignments Clear by Subject"),
+         caption = "") 
+    
+
+# By District
+
+TAMO %>%
+    filter(
+        Aggregate_Level == "D",
+        School_Grade_Span == "ALL",
+        Subject_Area == "TA",
+        Academic_Year == "2021-22"
+    ) %>%
+    lollipop(Clear_FTE_percent,
+             District_Name,
+             "steel blue") +
+    labs(x = "",
+         y = "",
+         color ="",
+         title = ("Teaching Assignments Clear by District"),
+         caption = "") 
